675d58df93f1e1f012f5820c2b83298bd894907c
Miracle <miraclepatidehen@gmail.com>
2023-04-20 13:14:29 +0100

    added data for creations

---
+++ data.js
@@ -0,0 +1,65 @@
+export const creations = [
+  {
+    id: 1,
+    text: 'deep earth',
+    images: {
+      mobile: './images/mobile/image-deep-earth.jpg',
+      desktop: './images/desktop/image-deep-earth.jpg',
+    },
+  },
+  {
+    id: 2,
+    text: 'night arcade',
+    images: {
+      mobile: './images/mobile/image-night-arcade.jpg',
+      desktop: './images/desktop/image-night-arcade.jpg',
+    },
+  },
+  {
+    id: 3,
+    text: 'soccer team',
+    images: {
+      mobile: './images/mobile/image-soccer-team.jpg',
+      desktop: './images/desktop/image-soccer-team.jpg',
+    },
+  },
+  {
+    id: 4,
+    text: 'the grid',
+    images: {
+      mobile: './images/mobile/image-grid.jpg',
+      desktop: './images/desktop/image-grid.jpg',
+    },
+  },
+  {
+    id: 5,
+    text: 'from up above vr',
+    images: {
+      mobile: './images/mobile/image-from-above.jpg',
+      desktop: './images/desktop/image-from-above.jpg',
+    },
+  },
+  {
+    id: 6,
+    text: 'pocket borealis',
+    images: {
+      mobile: './images/mobile/image-pocket-borealis.jpg',
+      desktop: './images/desktop/image-pocket-borealis.jpg',
+    },
+  },
+  {
+    id: 7,
+    text: 'the curiosity',
+    images: {
+      mobile: './images/mobile/image-curiosity.jpg',
+      desktop: './images/desktop/image-curiosity.jpg',
+    },
+  },
+  {
+    text: 'make it fish eye',
+    images: {
+      mobile: './images/mobile/image-fisheye.jpg',
+      desktop: './images/desktop/image-fisheye.jpg',
+    },
+  },
+]
