22ed4e014620e058886a28b618b82f24ec94a805
Miracle <miraclepatidehen@gmail.com>
2023-04-20 13:15:25 +0100

    added

---
+++ creations.js
@@ -0,0 +1,33 @@
+import { creations } from './data.js'
+
+const mobileDOM = document.querySelector('.creations.mobile .img-center')
+const desktopDOM = document.querySelector('.creations.desktop .img-center')
+
+const mobileView = creations
+  .map(({ id, text, images }) => {
+    const { mobile: img } = images
+    return ` 
+        <div id=${id}>
+          <img src=${img} alt=${text} />
+          <h3 class="img-text">${text}</h3>
+        </div> `
+  })
+  .join('')
+
+const desktopView = creations
+  .map(({ id, text, images }) => {
+    const { desktop: img } = images
+    return ` 
+        <div id=${id}>
+          <img src=${img} alt=${text} />
+          <h3 class="img-text">${text}</h3>
+        </div> `
+  })
+  .join('')
+
+const displayCreations = () => {
+  mobileDOM.innerHTML = mobileView
+  desktopDOM.innerHTML = desktopView
+}
+
+export default displayCreations
