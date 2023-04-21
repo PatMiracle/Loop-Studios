4c254608a34abff41e10bfc4a7b5fb249b293eb8
Miracle <miraclepatidehen@gmail.com>
2023-04-21 01:40:53 +0100

    removed scroll reveal

--- app.js
+++ app.js
@@ -13,23 +13,3 @@ sidebar.addEventListener('click', () => {
 
 // display creations
 window.addEventListener('DOMContentLoaded', displayCreations())
-
-// scroll animations
-ScrollReveal().reveal('.hero', {
-  origin: 'left',
-  distance: '40px',
-  delay: 500,
-  easing: 'ease-in',
-})
-ScrollReveal().reveal('img', {
-  origin: 'bottom',
-  distance: '40px',
-  delay: 300,
-  easing: 'ease-in',
-})
-ScrollReveal().reveal('article', {
-  origin: 'right',
-  distance: '40px',
-  delay: 300,
-  easing: 'ease-in',
-})
